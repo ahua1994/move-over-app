9a8fce2b07e507ed3277608c9abb9558d7fd2369
Andy Hua <user.andy.hua@gmail.com>
2023-06-16 18:43:53 -0700

    better navbar, still needs

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,9 @@
+"use client";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Image from "next/image";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,42 +13,47 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+    const nav = useRouter();
     return (
         <html lang="en">
-            <nav className="fixed w-full top-0 text-white flex justify-between">
+            <nav className="fixed w-full top-0 text-white flex justify-between h-20 ">
                 <Image
                     src="/moveoverlogo.png"
                     alt="Move Over Logo"
                     width="200"
                     height="150"
-                    className="m-0"
                 ></Image>
                 <div className="links flex mr-2">
-                    <Link href="/startmove">
-                        <button className="start align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
-                            Start A Move
-                        </button>
-                    </Link>
-                    <Link href="/mymoves">
-                        <button className="mymoves align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
-                            My Moves
-                        </button>
-                    </Link>
-                    <Link href="/about">
-                        <button className="about align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
-                            About
-                        </button>
-                    </Link>
-                    <Link href="/register">
-                        <button className="register align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
-                            Register
-                        </button>
-                    </Link>
-                    <Link href="/login">
-                        <button className="login align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
-                            Login
-                        </button>
-                    </Link>
+                    <button
+                        onClick={() => nav.push("/startmove")}
+                        className="start align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+                    >
+                        Start A Move
+                    </button>
+                    <button
+                        onClick={() => nav.push("/mymoves")}
+                        className="mymoves align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+                    >
+                        My Moves
+                    </button>
+                    <button
+                        onClick={() => nav.push("/about")}
+                        className="about align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+                    >
+                        About
+                    </button>
+                    <button
+                        onClick={() => nav.push("/register")}
+                        className="register align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+                    >
+                        Register
+                    </button>
+                    <button
+                        onClick={() => nav.push("/login")}
+                        className="login align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800"
+                    >
+                        Login
+                    </button>
                     <button className="logout align h-14 px-5 pb-1 mt-4 mx-2 text-gray-100 transition-colors duration-150 bg-gray-700 rounded-lg focus:shadow-outline hover:bg-gray-800">
                         Logout
                     </button>
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Register = () => {
-    return <div>Register</div>;
+    return (
+        <div className="Register flex justify-center items-center pt-24">
+            <div className="reg-form text-black bg-slate-500 w-[30%] text-center rounded-lg">
+                <h1>Register</h1>
+                {/* make better tailwind input/label */}
+                <form action="" className="flex flex-col">
+                    <label htmlFor="username">Username:</label>
+                    <input id="username" type="text" name="username" />
+                    <label htmlFor="email">Email:</label>
+                    <input id="email" type="text" name="email" />
+                    <label htmlFor="password">Password:</label>
+                    <input id="password" type="password" name="password" />
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default Register;
